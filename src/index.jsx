829eee3ee8bf22170d32b7e138971256902e4ef8
Alexis Canyon <acanyon@gmail.com>
2019-09-28 22:42:42 -0700

    semicolins

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {AppContainer} from 'react-hot-loader'
-import Routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+import Routes from './routes';
 
-import "styles/base/_main.sass"  // Global styles
-import "styles/base/_common.sass"  // Global styles
-import "styles/_style.sass"  // Css-module styles
+import "styles/base/_main.sass";  // Global styles
+import "styles/base/_common.sass";  // Global styles
+import "styles/_style.sass";  // Css-module styles
 
 import { Provider } from "react-redux";
 import store from "./stores/Root.js";
 
-const renderApp = (Component) => {
+function renderApp (Component) {
   ReactDOM.render(
     <Provider store={store}>
       <AppContainer>
