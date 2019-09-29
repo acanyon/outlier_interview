13e952194a47ab2fa4035e87093de0e34f3fef50
Alexis Canyon <acanyon@gmail.com>
2019-09-28 23:54:34 -0700

    fixed tests by adding the store

--- src/__tests__/app.test.jsx
+++ src/__tests__/app.test.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Routes from '../routes';
+import store from "../stores/Root.js";
+import { Provider } from "react-redux";
 
 configure({ adapter: new Adapter() });
 
 describe('app', () => {
   it('renders without crashing', () => {
-    mount(<Routes />);
+    mount(
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    );
   });
 });
